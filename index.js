e4059dda6b4dab488076cf8a1c8b096a9be873f0
Harley Guan <hguan@fortinet.com>
2019-07-16 16:34:33 -0700

    First version of ftnt-devops-ci tool

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+'use strict';
+/*
+ * ftnt-devops-ci
+ * A Node.js project format checking and linting tool for CI practices.
+ *
+ * Author: Harley Guan <guardhao@gmail.com>
+ *
+ */
+
+ 
