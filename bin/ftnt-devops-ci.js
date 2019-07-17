4b79783771096919a5db15b9d2acf86d643a0634
Harley Guan <hguan@fortinet.com>
2019-07-17 15:51:51 -0700

    Modify file path

--- bin/ftnt-devops-ci.js
+++ bin/ftnt-devops-ci.js
@@ -2,16 +2,17 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const sh = require('shelljs');
 const program = require('commander');
 const appInfo = require('../package.json');
 
-const relatedPath = './node_modules/ftnt-devops-ci';
-const prettierPath = `${process.cwd()}/node_modules/.bin/prettier`;
+const relatedPath = path.normalize(`${__dirname}/..`);
+const prettierPath = `${relatedPath}/node_modules/.bin/prettier`;
 const prettierConfigPath = fs.existsSync(`${process.cwd()}/.prettierrc`)
     ? `${process.cwd()}/.prettierrc`
     : `${relatedPath}/.prettierrc`;
-const eslintPath = `${process.cwd()}/node_modules/.bin/eslint`;
+const eslintPath = `${relatedPath}/node_modules/.bin/eslint`;
 const eslintConfigPath = fs.existsSync(`${process.cwd()}/.eslintrc`)
     ? `${process.cwd()}/.eslintrc`
     : `${relatedPath}/.eslintrc`;
@@ -38,6 +39,7 @@ program
             }
         }
         if (options.lint || no_options) {
+            console.log("Checking linting...");
             if (options.lint_ignore) {
                 sh.exec(
                     `${eslintPath} -c ${eslintConfigPath} --ignore-pattern ${options.lint_ignore} ${path}`
@@ -71,6 +73,7 @@ program
             }
         }
         if (options.lint || no_options) {
+            console.log("Fixing linting...");
             if (options.lint_ignore) {
                 sh.exec(
                     `${eslintPath} -c ${eslintConfigPath} --ignore-pattern ${options.lint_ignore} --fix ${path}`
